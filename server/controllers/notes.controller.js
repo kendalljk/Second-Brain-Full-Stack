a5efb5b358469f09ba4397fd13e189a7d39e9501
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-07-09 14:28:50 -0700

    organization

--- server/controllers/notes.controller.js
+++ server/controllers/notes.controller.js
@@ -1,38 +1,38 @@
 const Note = require("../models/note");
 
 exports.getNote = async (req, res, next) => {
-  try {
-    const note = await Note.findById(req.params.id);
-    res.json(note);
-  } catch (error) {
-    res.status(500).json({ error: "Unable to find note" });
-  }
+    try {
+        const note = await Note.findById(req.params.id);
+        res.json(note);
+    } catch (error) {
+        res.status(500).json({ error: "Unable to find note" });
+    }
 };
 
 exports.addNote = async (req, res, next) => {
+    console.log(req.body);
+    const { bookTitle, bookAuthor, bookSummary, bookGenre, bookFinished } =
+        req.body;
+    const bookQuotes = Array.isArray(req.body.bookQuotes)
+        ? req.body.bookQuotes
+        : [req.body.bookQuotes];
+    const bookNotes = Array.isArray(req.body.bookNotes)
+        ? req.body.bookNotes
+        : [req.body.bookNotes];
+    const coverPath = req.body.coverPath;
+
+    const newNote = new Note({
+        title: bookTitle,
+        author: bookAuthor,
+        cover: coverPath,
+        genre: bookGenre,
+        summary: bookSummary,
+        quotes: bookQuotes,
+        notes: bookNotes,
+        finished: bookFinished,
+    });
+
     try {
-        console.log(req.body);
-        const { bookTitle, bookAuthor, bookSummary, bookGenre, bookFinished } =
-            req.body;
-        const bookQuotes = Array.isArray(req.body.bookQuotes)
-            ? req.body.bookQuotes
-            : [req.body.bookQuotes];
-        const bookNotes = Array.isArray(req.body.bookNotes)
-            ? req.body.bookNotes
-            : [req.body.bookNotes];
-        const coverPath = req.body.coverPath;
-
-        const newNote = new Note({
-            title: bookTitle,
-            author: bookAuthor,
-            cover: coverPath,
-            genre: bookGenre,
-            summary: bookSummary,
-            quotes: bookQuotes,
-            notes: bookNotes,
-            finished: bookFinished,
-        });
-
         const savedNote = await newNote.save();
         res.json(savedNote);
     } catch (error) {
@@ -42,26 +42,29 @@ exports.addNote = async (req, res, next) => {
 };
 
 exports.deleteNote = async (req, res, next) => {
-  try {
-    const note = await Note.findByIdAndDelete(req.params.id)
-    if (!note) {
-      return res.status(404).json({ error: "Unable to find note" });
+    try {
+        const note = await Note.findByIdAndDelete(req.params.id);
+        if (!note) {
+            return res.status(404).json({ error: "Unable to find note" });
+        }
+        res.json({ message: "Successfully deleted note" });
+    } catch (error) {
+        res.status(500).json({ error: "Unable to delete note" });
     }
-    res.json({message: "Successfully deleted note"})
-  } catch (error) {
-    res.status(500).json({error: "Unable to delete note"})
-  }
-}
+};
 
 exports.editNote = async (req, res, next) => {
-  try {
-    const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    try {
+        const updatedNote = await Note.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
         if (!updatedNote) {
-      return res.status(404).json({ error: "Unable to find note" });
+            return res.status(404).json({ error: "Unable to find note" });
+        }
+        res.json(updatedNote);
+    } catch (error) {
+        res.status(500).json({ error: "Unable to edit note" });
     }
-    res.json(updatedNote);
-  } catch (error) {
-    res.status(500).json({ error: "Unable to edit note" });
-
-  }
-}
+};
